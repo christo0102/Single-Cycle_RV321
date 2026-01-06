e836c5549afbb65de05a1c7e1cf0ec60882e8122
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2026-01-06 17:27:12 +0530

    Create mux_alu_one.v

---
+++ RTL/mux_alu_one.v
@@ -0,0 +1,10 @@
+`timescale 1ns / 1ps  
+module mux_alu_srca( 
+input  [31:0] pc_out, 
+input  [31:0] rd1, 
+input         
+ALUSrcA, 
+output [31:0] A 
+); 
+assign A = (ALUSrcA) ? pc_out : rd1; 
+endmodule
