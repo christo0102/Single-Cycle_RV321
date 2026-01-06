5ea43bb084cb825f29558f5879d0fe0bc5698ea1
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2026-01-06 17:29:03 +0530

    Create data_memory.v

---
+++ RTL/data_memory.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps  
+module Data_Memory #(parameter DEPTH = 256)( 
+    input wire  clk,
+    input wire  MemWrite,
+    input wire [31:0]  ALU_Result,
+    input wire [31:0]  Write_Data,
+    output wire [31:0] Read_Data
+); 
+    reg [31:0] mem [0:DEPTH-1]; 
+    initial begin 
+        $readmemh("data.mem", mem); 
+    end 
+    assign Read_Data = mem[ALU_Result[9:2]]; 
+    always @(posedge clk) begin 
+        if (MemWrite) begin 
+            mem[ALU_Result[9:2]] <= Write_Data; 
+        end 
+    end 
+   
+endmodule
