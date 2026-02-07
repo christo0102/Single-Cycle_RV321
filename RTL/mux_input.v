289c42a9887c12d49d9e4321ea0bc3e51a378eb7
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2026-02-07 17:52:18 +0530

    mux_input.v

--- RTL/mux_input.v
+++ RTL/mux_input.v
@@ -1,18 +1,17 @@
-`timescale 1ns / 1ps  
-module mux_pc( 
-input  [31:0] pc_plus4, 
-input  [31:0] pc_target , 
-input  [31:0] Result, 
-input  [1:0]  PCSrc, 
-output reg [31:0] pc_next 
-); 
-always @(*) begin 
-case (PCSrc) 
-2'b00: pc_next = pc_plus4; 
-2'b01: pc_next = pc_target; 
-2'b10: pc_next = Result; 
-2'b11: pc_next = 32'b0; 
-default: pc_next = 32'b0; 
-endcase 
-end 
+module mux_pc(
+    input  [31:0] pc_plus4,
+    input  [31:0] pc_target,
+    input  [31:0] jalr_target,
+    input  [1:0]  PCSrc,
+    output reg [31:0] pc_next
+);
+always @(*) begin
+    case (PCSrc)
+        2'b00: pc_next = pc_plus4;
+        2'b01: pc_next = pc_target;
+        2'b10: pc_next = jalr_target;
+        2'b11: pc_next = 32'b0;
+        default: pc_next = pc_plus4;
+    endcase
+end
 endmodule
