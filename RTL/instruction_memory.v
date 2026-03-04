3b28410a72472733b621bf94972d830997563f98
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2026-03-04 22:28:29 +0530

    instruction_memory.v

--- RTL/instruction_memory.v
+++ RTL/instruction_memory.v
@@ -1,8 +1,8 @@
 `timescale 1ns / 1ps  
 module Instruction_Memory ( 
-    input  wire [31:0] pc_out, 
+    input  wire [7:0] pc_out, 
     output wire [31:0] instruction 
 ); 
     reg [31:0] inst_memory [0:255]; 
-      assign instruction = inst_memory[pc_out[9:2]]; 
+      assign instruction = inst_memory[pc_out[7:0]]; 
 endmodule
