e5462ec2ce53687962aac4510a30a63225edf277
Christo Augustine <149934273+christo0102@users.noreply.github.com>
2026-03-04 22:28:58 +0530

    immediate_generator.v

--- RTL/immediate_generator.v
+++ RTL/immediate_generator.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps  
 module Extend (
-    input  [31:0] instruction,
+    input  [24:0] instruction,
     input  [2:0]  ImmSrc,
     output reg [31:0] ImmExt
 );
@@ -8,19 +8,19 @@ module Extend (
     always @(*) begin
         case (ImmSrc)
             // I-Type (for addi, lw, jalr)
-            3'b000: ImmExt = { {20{instruction[31]}}, instruction[31:20] };
+            3'b000: ImmExt = { {20{instruction[24]}}, instruction[24:13] };
 
             // S-Type (for sw, sh, sb)
-            3'b001: ImmExt = { {20{instruction[31]}}, instruction[31:25], instruction[11:7] };
+            3'b001: ImmExt = { {20{instruction[24]}}, instruction[24:18], instruction[4:0] };
 
             // B-Type (for beq, bne, etc.)
-            3'b010: ImmExt = { {20{instruction[31]}}, instruction[7], instruction[30:25], instruction[11:8], 1'b0 };
+            3'b010: ImmExt = { {20{instruction[24]}}, instruction[0], instruction[23:18], instruction[4:1], 1'b0 };
 
             // J-Type (for jal)
-            3'b011: ImmExt = { {11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0 };
+            3'b011: ImmExt = { {11{instruction[24]}}, instruction[24], instruction[12:5], instruction[13], instruction[23:14], 1'b0 };
 
             // U-Type (for lui, auipc)
-            3'b100: ImmExt = { instruction[31:12], 12'b0 };
+            3'b100: ImmExt = { instruction[24:5], 12'b0 };
 
             // Default case to prevent latches and provide a known safe value
             default: ImmExt = 32'h00000000;
